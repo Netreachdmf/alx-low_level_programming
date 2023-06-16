3ce552d1d92e104eccc1f819cc1b1f5971cef9f2
Iheme Chigozie Solomon <chigzscale@gmail.com>
2023-06-15 18:02:24 -0700

    UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - program that prints with an output error
+ *
+ * Return 1  Error
+ */
+
+int main(void)
+{
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+This is the Readme file for learning C language. This repo will contain all the projects for the C language
