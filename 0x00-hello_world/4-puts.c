49eedd1e2f28033703a914952eb33736d6b5e401
Iheme Chigozie Solomon <chigzscale@gmail.com>
2023-06-15 17:14:44 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * This program demonstrates the usage of the puts function
+ * main - this serves as the entry point of the program
+ * Return : indicates successful program execution
+ */
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
