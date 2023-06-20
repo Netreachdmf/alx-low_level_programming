1be7cd872faa22ae8cebccca5298bd93d56764a8
Iheme Chigozie Solomon <chigzscale@gmail.com>
2023-06-20 02:58:57 -0700

    101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,12 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
  * main - program that prints with an output error
  *
- * Return 1 (Error)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
