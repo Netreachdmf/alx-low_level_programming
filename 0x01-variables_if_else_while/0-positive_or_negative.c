99d75520b52d763d65f652c3c8424e5fcfb37d61
Iheme Chigozie Solomon <chigzscale@gmail.com>
2023-06-20 05:43:43 -0700

    0-positive_or_negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,7 +12,7 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
-printf("%d\n is positive\n", n);
+printf("%d\n is positive", n);
 }
 else if (n < 0)
 {
@@ -20,7 +20,7 @@ printf("%d\n is negative\n", n);
 }
 else
 {
-printf("%d\n is zero\n", n);
+printf("%d\n is zero", n);
 }
 return (0);
 }
